46810067d8b41d2832541f68223c0cb2e2431db0
Logan Shire <logan.shire@gmail.com>
2023-06-01 17:48:36 -0400

    Update package.swift

--- Package.swift
+++ Package.swift
@@ -29,6 +29,15 @@ let package = Package(
             exclude: ["BUILD.bazel"]
         ),
         .target(
+            name: "examples_xplatform_grpc_echo_server_services_swift",
+            dependencies: [
+                .product(name: "GRPC", package: "grpc-swift"),
+                .target(name: "examples_xplatform_grpc_echo_proto")
+            ],
+            path: "examples/xplatform/examples_xplatform_grpc_echo_server_services_swift",
+            exclude: ["BUILD.bazel"]
+        ),
+        .target(
             name: "examples_xplatform_grpc_echo_client_test_stubs_swift",
             dependencies: [
                 .product(name: "GRPC", package: "grpc-swift"),
@@ -44,7 +53,7 @@ let package = Package(
                 .product(name: "GRPC", package: "grpc-swift"),
                 .target(name: "examples_xplatform_grpc_echo_proto"),
                 .target(name: "examples_xplatform_grpc_echo_client_services_swift"),
-                .target(name: "examples_xplatform_grpc_echo_client_test_stubs_swift"),
+                .target(name: "examples_xplatform_grpc_echo_server_services_swift"),
             ],
             path: "examples/xplatform/examples_xplatform_grpc_echo_client_unit_test",
             exclude: [
